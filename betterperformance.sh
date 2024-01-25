8f287a20f064a1f6907d1fce9c4c2a02ec502098
lardex668 <110268771+lardex668@users.noreply.github.com>
2024-01-25 21:06:04 +0100

    Delete betterperformance.sh

--- betterperformance.sh
+++
@@ -1,9 +0,0 @@
-# Reduce motion
-sudo defaults write com.apple.universalaccess reduceMotion -bool true
-
-# Set transparency off
-sudo defaults write com.apple.universalaccess reduceTransparency -bool true
-
-# Restart Dock and Finder to apply changes
-sudo killall Dock
-sudo killall Finder
