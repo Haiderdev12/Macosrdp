b5bdf4513fdeee4fb90382fcfbae1c59a78c3185
lardex668 <110268771+lardex668@users.noreply.github.com>
2024-01-25 19:54:39 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -59,15 +59,6 @@ fi
 # Verifica la versione di Python
 python --version
 
-# Scarica lo script display_manager.py da GitHub
-git clone https://github.com/univ-of-utah-marriott-library-apple/display_manager
-
-# Entra nella cartella dello script
-cd display_manager
-
-# Imposta la risoluzione a 1280x720 (scalata) su tutti gli schermi
-python display_manager.py -s 1280 720
-
 #install ngrok
 brew install --cask ngrok
 
