d2b4d23c401ec0d4db04eb9cd6544fa247947992
lardex668 <110268771+lardex668@users.noreply.github.com>
2024-01-28 04:09:46 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -11,57 +11,6 @@ sudo dscl . -passwd /Users/lardex $1
 sudo dscl . -passwd /Users/lardex $1
 sudo createhomedir -c -u lardex > /dev/null
 
-# Install MacPorts
-echo "Installing MacPorts..."
-curl -O https://distfiles.macports.org/MacPorts/MacPorts-2.9.0.tar.bz2
-tar xf MacPorts-2.9.0.tar.bz2
-cd MacPorts-2.9.0/
-sudo chmod +x ./configure
-./configure
-sudo make
-sudo make install
-cd ..
-rm -rf MacPorts-2.9.0*
-echo "MacPorts installed successfully."
-
-# Update PATH for MacPorts
-echo "Updating PATH for MacPorts..."
-echo 'export PATH="/opt/local/bin:/opt/local/sbin:$PATH"' >> ~/.bash_profile
-source ~/.bash_profile
-
-# Install xprop
-echo "Installing xprop..."
-sudo port install xorg-xprop
-echo "xprop installed successfully."
-
-# Install XQuartz
-echo "Installing XQuartz..."
-brew install --cask xquartz
-
-# Start XQuartz
-echo "Starting XQuartz..."
-open -a XQuartz
-
-# Set DISPLAY environment variable
-echo "Setting DISPLAY environment variable..."
-export DISPLAY=:0
-
-# Install FreeRDP
-echo "Installing FreeRDP..."
-brew install freerdp
-
-# Define the rule
-rule="pass in proto tcp from any to any port 3389"
-
-# Write the rule to a file
-sudo echo "$rule" > /tmp/com.apple.alf.plist
-
-# Load the rule into the firewall
-sudo pfctl -e -f /tmp/com.apple.alf.plist
-
-# Start the FreeRDP server
-echo "Starting the FreeRDP server..."
-xfreerdp /server &
 
 #install ngrok
 brew install --cask ngrok
