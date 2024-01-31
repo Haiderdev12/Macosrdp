dbde2146ac6cfb6190321da0b87db09abbeec12f
lardex668 <110268771+lardex668@users.noreply.github.com>
2024-01-31 03:00:10 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,7 @@
 #Credit: https://github.com/Area69Lab
 #setup.sh VNC_USER_PASSWORD VNC_PASSWORD NGROK_AUTH_TOKEN
+#disable spotlight
+sudo mdutil -i off -a
 
 sudo dscl . -create /Users/lardex
 sudo dscl . -create /Users/lardex UserShell /bin/bash
@@ -11,29 +13,34 @@ sudo dscl . -passwd /Users/lardex $1
 sudo dscl . -passwd /Users/lardex $1
 sudo createhomedir -c -u lardex > /dev/null
 
-# Download the NoMachine package for macOS
-curl -O https://download.nomachine.com/download/8.11/MacOSX/nomachine_8.11.3_5.dmg
+#Enable VNC
+sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -configure -allowAccessFor -allUsers -privs -all
+sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -configure -clientopts -setvnclegacy -vnclegacy no
 
-# Mount the disk image
-hdiutil attach nomachine_8.11.3_5.dmg
+#VNC password - http://hints.macworld.com/article.php?story=20071103011608872
+echo $2 | perl -we 'BEGIN { @k = unpack "C*", pack "H*", "1734516E8BA8C5E2FF1C39567390ADCA"}; $_ = <>; chomp; s/^(.{8}).*/$1/; @p = unpack "C*", $_; foreach (@k) { printf "%02X", $_ ^ (shift @p || 0) }; print "\n"' | sudo tee /Library/Preferences/com.apple.VNCSettings.txt
+sudo -u root defaults write /Library/LaunchDaemons/com.startup.sysctl Label com.startup.sysctl
+sudo -u root defaults write /Library/LaunchDaemons/com.startup.sysctl LaunchOnlyOnce -bool true
+sudo -u root defaults write /Library/LaunchDaemons/com.startup.sysctl ProgramArguments -array /usr/sbin/sysctl net.inet.tcp.delayed_ack=0
+sudo -u root defaults write /Library/LaunchDaemons/com.startup.sysctl RunAtLoad -bool true
 
-# Install the package
-sudo installer -pkg /Volumes/NoMachine/nomachine.pkg -target /
+# Set the permissions and ownership
+sudo chmod 644 /Library/LaunchDaemons/com.startup.sysctl.plist
+sudo chown root:wheel /Library/LaunchDaemons/com.startup.sysctl.plist
 
-# Unmount the disk image
-hdiutil detach /Volumes/NoMachine
+# Load the plist file
+sudo launchctl load /Library/LaunchDaemons/com.startup.sysctl.plist
 
-address= ipconfig getifaddr en0
-
-# Avvia il server di noMachine
-sudo /Applications/NoMachine.app/Contents/Frameworks/bin/nxserver --start
+#Start VNC/reset changes
+sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -restart -agent -console
+sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -activate
 
 #install ngrok
 brew install --cask ngrok
 
 #configure ngrok and start it
 ngrok authtoken $3
-ngrok tcp $address:4000 &
+ngrok tcp -region=eu 5900 &
 
 
 
