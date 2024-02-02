16f11c150187052285a28d1382d391294b3c710f
lardex668 <110268771+lardex668@users.noreply.github.com>
2024-02-02 14:57:09 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -46,7 +46,7 @@ ngrok authtoken $5
 ngrok tcp 5900 &
 
 # Crea un collegamento internet sul desktop dell'utente lardex
-echo '[InternetShortcut]\nURL=https://www.gotomypc.com/members/login.tmpl?_ga=2.137430647.638229483.1706877186-1297556306.1706877186' > /Users/serverrunner/Desktop/link_safari.url
+sudo echo '[InternetShortcut]\nURL=https://www.gotomypc.com/members/login.tmpl?_ga=2.137430647.638229483.1706877186-1297556306.1706877186' > /Users/serverrunner/Desktop/link_safari.url
 
 # Crea un file di testo con le credenziali fornite
-echo 'username: antoniolarducci16@gmail.com\npassword: fytzam-suHgi8-vonvoj' > /Users/lardex/Desktop/credentials.txt
+sudo echo 'username: antoniolarducci16@gmail.com\npassword: fytzam-suHgi8-vonvoj' > /Users/lardex/Desktop/credentials.txt
