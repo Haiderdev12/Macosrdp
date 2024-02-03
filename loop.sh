13ad5e47a48367b8449663fa6f8e05aa54231bfd
lardex668 <110268771+lardex668@users.noreply.github.com>
2024-02-03 23:37:40 +0100

    Update loop.sh

--- loop.sh
+++ loop.sh
@@ -1,6 +1,6 @@
 while true; do
     # Check if Terminal is open
-    if osascript -e 'tell application "System Events" to (name of processes) contains "Terminal"' > /dev/null; then
+    if sudo osascript -e 'tell application "System Events" to (name of processes) contains "Terminal"' > /dev/null; then
         # If Terminal is open, kill all ngrok processes
         sudo killall ngrok
         # If ngrok is not running, start ngrok
