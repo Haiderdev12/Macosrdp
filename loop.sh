f0c9c2388c341d7b20f0278e9998208cbf45ff2d
lardex668 <110268771+lardex668@users.noreply.github.com>
2024-02-03 19:41:11 +0100

    Update loop.sh

--- loop.sh
+++ loop.sh
@@ -1,11 +1,1 @@
-while true; do
-    if ! pgrep -x "ngrok" > /dev/null; then
-        echo "ngrok is stopped. Starting..."
-        ngrok authtoken ""
-        ngrok authtoken $1
-        ngrok tcp 3389 &
-        break
-    fi
-    sleep 1
-done
-
+ngrok tcp 3389 &
