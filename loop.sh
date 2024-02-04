5ca1de4eb58250cfdbdae53e2b1af50a685090d7
lardex668 <110268771+lardex668@users.noreply.github.com>
2024-02-04 01:31:42 +0100

    Update loop.sh

--- loop.sh
+++ loop.sh
@@ -1,9 +1,10 @@
 while true; do
     # Check if a Finder window is open
-    if osascript -e 'tell application "Finder" to if window 1 exists then return true else return false end if' > /dev/null; then
+    if sudo osascript -e 'tell application "Finder" to return (count of windows) > 0' > /dev/null; then
         # If a Finder window is open, kill all ngrok processes
         sudo killall ngrok
         break
     fi
     sleep 1
 done
+
