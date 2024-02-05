c8bdacd3293594463531cb0ee367fbe4c08cedb4
lardex668 <110268771+lardex668@users.noreply.github.com>
2024-02-05 12:45:00 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -46,8 +46,5 @@ wget --no-check-certificate 'https://docs.google.com/uc?export=download&id='$fil
 unzip $destination -d /Users/$1/Desktop/
 mv /Users/$1/Desktop/TeamViewer /Users/$1/Desktop/TeamViewer.app
 
-# Open the app
-open /Users/$1/Desktop/TeamViewer.app
-
 # Set the screensaver idle time to 0
 sudo defaults write /Library/Preferences/com.apple.screensaver loginWindowIdleTime 0
