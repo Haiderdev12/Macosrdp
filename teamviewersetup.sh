c2956245e00c5e33f45ee26678b50ca96a541c0d
lardex668 <110268771+lardex668@users.noreply.github.com>
2024-02-05 13:33:51 +0100

    Update teamviewersetup.sh

--- teamviewersetup.sh
+++ teamviewersetup.sh
@@ -3,8 +3,14 @@ username=$1
 
 # Download the file from Google Drive
 file_id="1saqhkHbhDcz4uF66asQVu6rYsYsvPCne"
-destination="/Users/$1/Desktop/TeamViewer.app.zip"
+destination="/Users/$username/Desktop/TeamViewer.app.zip"
 sudo wget --no-check-certificate 'https://docs.google.com/uc?export=download&id='$file_id -O $destination
 
 # Set the screensaver idle time to 0
 sudo defaults write /Library/Preferences/com.apple.screensaver loginWindowIdleTime 0
+
+# Unzip the downloaded file
+sudo unzip /Users/$username/Desktop/TeamViewer.app.zip -d /Users/$username/Desktop/
+
+# Remove the zip file
+sudo rm /Users/$username/Desktop/TeamViewer.app.zip
