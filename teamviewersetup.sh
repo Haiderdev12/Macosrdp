377efe8211e4cd410d960a5d349f6b3db6616379
lardex668 <110268771+lardex668@users.noreply.github.com>
2024-02-05 14:20:56 +0100

    Update teamviewersetup.sh

--- teamviewersetup.sh
+++ teamviewersetup.sh
@@ -4,20 +4,5 @@ username=$1
 # Download the TeamViewer Host
 curl -L -o /tmp/TeamViewerHost.dmg "https://download.teamviewer.com/download/TeamViewerHost.dmg"
 
-# Mount the disk image and capture the output
-output=$(hdiutil attach /tmp/TeamViewerHost.dmg)
-
-# Extract the mount path from the output
-mount_path=$(echo "$output" | grep "/Volumes/" | awk '{print $3}')
-
-# Get the pkg name
-pkg_name=$(ls "$mount_path" | grep .pkg)
-
-# Install the pkg
-sudo installer -pkg "$mount_path/$pkg_name" -target /
-
-# Unmount the disk image
-hdiutil detach "$mount_path"
-
-# Move the application to the user's Desktop
-sudo mv /Applications/TeamViewerHost.app /Users/$username/Desktop/
+# Move the .dmg file to the user's Desktop
+sudo mv /tmp/TeamViewerHost.dmg /Users/$username/Desktop/
