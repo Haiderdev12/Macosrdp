f5f9f8dfb8e7cf02b544f5be89862f546e2ae238
lardex668 <110268771+lardex668@users.noreply.github.com>
2024-02-06 10:07:57 +0100

    Update teamviewersetup.sh

--- teamviewersetup.sh
+++ teamviewersetup.sh
@@ -1,46 +1,20 @@
 # Set the username
 username=$1
 
-# Set the download URL for TeamViewer
-url="https://download.teamviewer.com/download/TeamViewerHost.dmg"
+# Google Drive file ID
+FILE_ID="19ZV1COWjgQwlLRIuOQZBax9oIZlAf6oW"
+# Original file name
+FILE_NAME="TeamViewerHost.zip"
 
-# Set the destination path
-dest="/Users/$username/Desktop"
+# Destination path
+DEST_PATH="/Users/$1/Desktop/${FILE_NAME}"
 
-# Download TeamViewer
-echo "Downloading TeamViewer..."
-sudo curl -L $url -o $dest/TeamViewer.dmg
+# Google Drive URL
+URL="https://drive.google.com/uc?export=download&id=${FILE_ID}"
 
-# Mount the dmg file
-echo "Mounting the dmg file..."
-mount_output=$(sudo hdiutil attach $dest/TeamViewer.dmg)
+# Use curl to download
+sudo curl -L -o "${DEST_PATH}" "${URL}"
 
-# Get the mount point
-mount_point=$(echo $mount_output | grep -o '/Volumes/.*' | awk '{print $1}')
-
-# Get the name of the app
-app_name=$(ls $mount_point | grep '.app')
-
-# Move the TeamViewer app to the Desktop
-echo "Moving TeamViewer to the Desktop..."
-sudo cp -R "$mount_point/$app_name" $dest/
-
-# Unmount the dmg file
-echo "Unmounting the dmg file..."
-sudo hdiutil detach $mount_point
-
-echo "TeamViewer has been successfully installed on the Desktop."
-
-
-# Check if TeamViewerHost.app exists in the Applications folder
-while true; do
-    if [[ -d "/Applications/TeamViewerHost.app" ]]; then
-        sudo mv "/Applications/TeamViewerHost.app" "/Users/$1/Desktop/"
-        break
-    else
-        sleep 5
-    fi
-done
 
 sudo defaults write /Library/Preferences/com.apple.screensaver loginWindowIdleTime 0 
 
